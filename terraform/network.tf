b27a69df3da2c82f9db7c5c58cdde1b8b3b17415
Ritik Gupta <ritikgupta3@kpmg.com>
2024-01-24 12:41:55 +0530

    Pass rg name properly

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,14 +1,14 @@
 resource "azurerm_virtual_network" "this" {
   name                = "postgres-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_subnet" "this" {
   name                 = "postgres-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = ["10.0.1.0/24"]
 
   private_link_service_network_policies_enabled = false
